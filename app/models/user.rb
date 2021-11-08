b6eb825d8592a37f4abfbcfa75f4b392b1db2001
swaggingprogram <ogura0908work@gmail.com>
2021-11-08 12:55:36 +0900

    サインアップ時のバリデーションの設定

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,9 +5,12 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
 
   validates :nickname, presence: true
-  validates :kanji_last, presence: true
-  validates :kanji_first, presence: true
-  validates :kana_last, presence: true
-  validates :kana_first, presence: true
+  validates :kanji_last, presence: true, format: {with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/, message: "名字は漢字で入力してください"}
+  validates :kanji_first, presence: true, format: {with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/, message: "名前は漢字で入力してください"}
+  validates :kana_last, presence: true, format: { with: /\A[ァ-ヶー－]+\z/, message: "は全角カタカナで入力して下さい" }
+  validates :kana_first, presence: true, format: { with: /\A[ァ-ヶー－]+\z/, message: "カタカナで入力してください"}
   validates :birth, presence: true
+
+  validates :password, format: {with: /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i, message: "パスワードは英数混合で入力してください"}
+  validates :password_confirmation, format: {with: /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i, message:  "パスワードは英数混合で入力してください"}
 end
