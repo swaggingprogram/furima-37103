51bc1d83436a7f30dfdd802ce185dc52c5ccbf6e
swaggingprogram <ogura0908work@gmail.com>
2021-11-10 12:11:47 +0900

    ルートおよびコントローラーの処理を設定

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,5 +1,6 @@
 class ItemsController < ApplicationController
   before_action :authenticate_user!, except: [:index, :show]
+  before_action :item_guard, only: [:edit]
 
   def index
     @items = Item.all.order('created_at DESC')
@@ -19,6 +20,16 @@ class ItemsController < ApplicationController
   end
 
   def edit
+    @item = Item.find(params[:id])
+  end
+  
+  def update
+    @item = Item.find(params[:id])
+    if @item.update(item_params)
+      redirect_to root_path
+    else
+      render :edit
+    end
   end
 
   def show
@@ -30,5 +41,12 @@ class ItemsController < ApplicationController
   def item_params
     params.require(:item).permit(:name, :content, :category_id, :status_id, :fee_id, :place_id, :arrive_id, :price,
                                  :image).merge(user_id: current_user.id)
+  end
+
+  def item_guard
+    @item = Item.find(params[:id])
+    unless @item.user.id == current_user.id
+      redirect_to action: :index
+    end
   end
 end
