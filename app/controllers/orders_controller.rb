74197495a2b8b5d3fd023e98109d19fe4475b852
swaggingprogram <ogura0908work@gmail.com>
2021-11-15 14:51:12 +0900

    クレジット決済の実装

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -6,18 +6,25 @@ class OrdersController < ApplicationController
 
   def create
     @record_order = RecordOrder.new(order_params)
+    @item = Item.find(params[:item_id])
+    binding.pry
     if @record_order.valid?
+      Payjp.api_key = "sk_test_845dc5947b7c07ab2329b71c"
+      Payjp::Charge.create(
+        amount: @item.price,
+        card: order_params[:token],
+        currency: 'jpy'
+      )
       @record_order.save
       redirect_to root_path
     else 
       render :index
     end
-  end
+  end  
 
   private
   def order_params
     @item = Item.find(params[:item_id])
-    params.require(:record_order).permit(:postal_code, :prefecture_id, :city, :port, :building, :tel).merge(user_id: current_user.id, item_id: @item.id)
+    params.require(:record_order).permit(:postal_code, :prefecture_id, :city, :port, :building, :tel).merge(user_id: current_user.id, item_id: @item.id, token: params[:token])
   end
 end
-
--- app/models/record_order.rb
+++ app/models/record_order.rb
@@ -1,6 +1,6 @@
 class RecordOrder
   include ActiveModel::Model
-  attr_accessor :postal_code, :prefecture_id, :city, :port, :building, :tel, :user_id, :item_id
+  attr_accessor :postal_code, :prefecture_id, :city, :port, :building, :tel, :user_id, :item_id, :token
 
   with_options presence: true do
     validates :postal_code, format: {with: /\A[0-9]{3}-[0-9]{4}\z/, message: "is invalid. Include hyphen(-)"}
